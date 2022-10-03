62599a9aac9691ef2b16c03b400f07281d15e36a
Alex Todeush <83349347+alex-todeush@users.noreply.github.com>
2022-10-03 13:36:10 +0100

    Added list of error states to check

--- app.py
+++ app.py
@@ -5,22 +5,31 @@ app = Flask(__name__)
 #Prompts user to enter expressions to calculate until they exit
 def main():
     user_input = input("Please enter the expression you wish to calculate: ")
-    print(user_input)
     while (user_input != "exit"):
         print(calculate(user_input))
 
         user_input = input("Please enter the expression you wish to calculate: ")
-        print(user_input)
 
 #Takes a string and returns whether it is valid input to be calculated
 def validate_input(expression):
+    error_out = ""
+    #Error States:
+    #Unacceptable character (anything other than 0-9 or +-*)
+
+    #Duplicate operation
+
+    #Expression begins with operation
+
+    #Expression ends with operation
+
     return True
  
 #Takes an expression and returns the value    
 def calculate(numbers):
-    if validate_input(numbers) == True:
+    validation_result = validate_input(numbers)
+    if validation_result == True:
         return str(eval(numbers))
     else:
-        return "Invalid Input" 
+        return validation_result
 
 if __name__ == "__main__": main()
