e44ff462ab481471c0cea46b9454f33056462a05
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2022-10-03 09:22:49 +0100

    added actual tests to project on the 'calculate' function

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
 def hello_world():
+    return "Hello, World!"
+    
+def calculate(numbers):
     return "Hello, World!"
--- test_main.py
+++ test_main.py
@@ -1,5 +1,27 @@
-from app import hello_world
+from app import hello_world, calculate
 
 def test_hello_world():
     response = hello_world();
     assert response == "Hello, World!"
+    
+    
+def test_with_all_three_operands():
+    response = calculate("14+39-15*10+50");
+    assert response == "-47"
+    
+def test_with_addition_and_subtraction_operands():
+    response = calculate("564+642-345-613");
+    assert response == "248"
+    
+def test_with_multiplication():
+    response = calculate("87*23*53");
+    assert response == "106053"
+    
+# ERROR CHECKING
+def test_with_unexpected_string_char():
+    response = calculate("87t23*53");
+    assert response == "error: unexpected string character: t"
+    
+def test_with_duplicated_operation():
+    response = calculate("87+23**53");
+    assert response == "error: duplicate operation: *"
